8e70a1bf27ec29691168d1c70bb2abdf177d586b
William <william.gates.3299@gmail.com>
2024-08-17 21:30:23 +0330

    Implemented Music Player Screen Logic

--- app/src/main/java/com/xeniac/chillclub/feature_music_player/data/remote/dto/GetRadiosResponseDto.kt
+++ app/src/main/java/com/xeniac/chillclub/feature_music_player/data/remote/dto/GetRadiosResponseDto.kt
@@ -1,3 +1,15 @@
 package com.xeniac.chillclub.feature_music_player.data.remote.dto
 
-class GetRadiosResponseDto : ArrayList<RadioDto>()
+import com.xeniac.chillclub.feature_music_player.domain.models.GetRadiosResponse
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetRadiosResponseDto(
+    @SerialName("data")
+    val radioDtos: List<RadioDto>
+) {
+    fun toGetRadiosResponse(): GetRadiosResponse = GetRadiosResponse(
+        radios = radioDtos.map { it.toRadio() }
+    )
+}
---
+++ app/src/main/java/com/xeniac/chillclub/feature_music_player/domain/models/GetRadiosResponse.kt
@@ -0,0 +1,14 @@
+package com.xeniac.chillclub.feature_music_player.domain.models
+
+import android.os.Parcelable
+import com.xeniac.chillclub.feature_music_player.data.remote.dto.GetRadiosResponseDto
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class GetRadiosResponse(
+    val radios: List<Radio>
+) : Parcelable {
+    fun toGetRadiosResponseDto(): GetRadiosResponseDto = GetRadiosResponseDto(
+        radioDtos = radios.map { it.toRadioDto() }
+    )
+}
