512b8e5cc2b6c7796b593aa449cddf23402d8208
William <william.gates.3299@gmail.com>
2024-08-30 16:40:18 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/chillclub/core/data/utils/DateHelper.kt
+++ app/src/main/java/com/xeniac/chillclub/core/data/utils/DateHelper.kt
@@ -2,11 +2,11 @@ package com.xeniac.chillclub.core.data.utils
 
 import kotlinx.datetime.Clock
 
-typealias UnixTimeInMillis = Long
+typealias UnixTimeInMs = Long
 typealias UnixTimeInSeconds = Long
 
 object DateHelper {
-    fun getCurrentTimeInMillis(): UnixTimeInMillis = Clock.System.now().toEpochMilliseconds()
+    fun getCurrentTimeInMs(): UnixTimeInMs = Clock.System.now().toEpochMilliseconds()
 
     fun getCurrentTimeInSeconds(): UnixTimeInSeconds = Clock.System.now().epochSeconds
 }
