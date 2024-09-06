d25289688c650ba7e2716832580ba49a98859a22
William <william.gates.3299@gmail.com>
2024-09-06 17:06:15 +0330

    Minor Improvements

--- app/src/main/java/com/xeniac/chillclub/core/presentation/utils/UiEvent.kt
+++ app/src/main/java/com/xeniac/chillclub/core/presentation/utils/UiEvent.kt
@@ -1,11 +1,13 @@
 package com.xeniac.chillclub.core.presentation.utils
 
+import com.xeniac.chillclub.core.ui.navigation.Screen
+
 sealed class UiEvent : Event() {
     data class ShowShortSnackbar(val message: UiText) : UiEvent()
     data class ShowLongSnackbar(val message: UiText) : UiEvent()
     data class ShowActionSnackbar(val message: UiText) : UiEvent()
     data class ShowOfflineSnackbar(val message: UiText) : UiEvent()
 
-    data class Navigate(val route: String) : UiEvent() // TODO: CHANGE PARAM
+    data class Navigate(val screen: Screen) : UiEvent()
     data object NavigateUp : UiEvent()
 }
