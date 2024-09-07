bff2e9497e0ed9ec013adf67b77fe9b76505e2bd
William <william.gates.3299@gmail.com>
2024-09-07 18:52:24 +0330

    Implemented Music Player Screen Logic

--- app/src/main/java/com/xeniac/chillclub/core/data/utils/MathHelper.kt
+++ app/src/main/java/com/xeniac/chillclub/core/data/utils/MathHelper.kt
@@ -3,4 +3,4 @@ package com.xeniac.chillclub.core.data.utils
 fun Int.convertToPercentage(
     minValue: Int,
     maxValue: Int
-): Float = (this - minValue) / (maxValue - minValue) * 100f
+): Float = (this - minValue).toFloat() / (maxValue - minValue).toFloat() * 100f
