d483c1f67de93111648d6bb9f92b7b54e4039a63
William <william.gates.3299@gmail.com>
2024-09-07 18:34:26 +0330

    Implemented Music Player Screen Logic

--- app/src/main/java/com/xeniac/chillclub/feature_music_player/presensation/states/MusicPlayerState.kt
+++ app/src/main/java/com/xeniac/chillclub/feature_music_player/presensation/states/MusicPlayerState.kt
@@ -2,16 +2,18 @@ package com.xeniac.chillclub.feature_music_player.presensation.states
 
 import android.os.Parcelable
 import com.xeniac.chillclub.core.domain.models.RadioStation
-import com.xeniac.chillclub.feature_music_player.domain.repositories.MusicVolume
+import com.xeniac.chillclub.feature_music_player.domain.repositories.MusicVolumePercentage
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class MusicPlayerState(
+    val isRadioStationsLoading: Boolean = false,
+    val isMusicBuffering: Boolean = false,
+    val isMusicPlaying: Boolean = false,
+    val musicVolumePercentage: MusicVolumePercentage? = null,
     val currentRadioStations: RadioStation? = null,
     val radioStations: List<RadioStation> = emptyList(),
-    val isRadioStationsLoading: Boolean = false,
-    val musicVolume: MusicVolume? = null,
+    val isPermissionDialogVisible: Boolean = false,
     val notificationPermissionCount: Int = 0,
-    val permissionDialogQueue: List<String> = emptyList(),
-    val isPermissionDialogVisible: Boolean = false
+    val permissionDialogQueue: List<String> = emptyList()
 ) : Parcelable
