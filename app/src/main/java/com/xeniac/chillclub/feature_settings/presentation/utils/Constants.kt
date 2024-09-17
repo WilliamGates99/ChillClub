4071329d59abdaa8c0c43564142d111a0fcc8f45
William <william.gates.3299@gmail.com>
2024-09-17 19:55:29 +0330

    Updated Privacy Policy URL

--- app/src/main/java/com/xeniac/chillclub/feature_settings/presentation/utils/Constants.kt
+++ app/src/main/java/com/xeniac/chillclub/feature_settings/presentation/utils/Constants.kt
@@ -9,5 +9,5 @@ object Constants {
     const val URL_SOURCE = "https://github.com/WilliamGates99/ChillClub"
     const val URL_DONATE = "https://xeniacdev.github.io/donate"
     const val URL_ASK_QUESTION = "https://github.com/WilliamGates99/ChillClub/issues"
-    const val URL_PRIVACY_POLICY = "https://xeniacdev.github.io/donate" // TODO: UPDATE URL
+    const val URL_PRIVACY_POLICY = "https://xeniacdev.github.io/chillclub/privacy_policy"
 }
