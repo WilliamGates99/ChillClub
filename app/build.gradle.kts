26e221c0bdf68fe4957bc17bbd4555874ba8bbed
William <william.gates.3299@gmail.com>
2024-09-20 20:31:23 +0330

    v1.0.1 (2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.xeniac.chillclub"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         // Keeps language resources for only the locales specified below.
         resourceConfigurations.addAll(listOf("en-rUS"))
