82899704a942b5948f16a37cb9a10a79b55831fd
William <william.gates.3299@gmail.com>
2026-02-03 20:21:00 +0330

    v1.1.3 (6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.xeniac.chillclub"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "com.xeniac.chillclub.HiltTestRunner"
 
