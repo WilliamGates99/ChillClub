7359cdd81883f3800805e5462ef27e10517e8800
William <william.gates.3299@gmail.com>
2026-03-17 14:04:37 +0330

    Updated domains

--- app/src/main/java/com/xeniac/chillclub/feature_settings/presentation/utils/Constants.kt
+++ app/src/main/java/com/xeniac/chillclub/feature_settings/presentation/utils/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
     // Web URLs
     const val URL_ABOUT_US = "https://xeniac.ir"
     const val URL_SOURCE = "https://github.com/WilliamGates99/ChillClub"
-    const val URL_DONATE = "https://xeniacdev.github.io/donate"
+    const val URL_DONATE = "https://donate.xeniac.ir"
     const val URL_ASK_QUESTION = "https://github.com/WilliamGates99/ChillClub/issues"
-    const val URL_PRIVACY_POLICY = "https://xeniacdev.github.io/chillclub/privacy_policy"
+    const val URL_PRIVACY_POLICY = "https://chillclub.xeniac.ir/privacy_policy"
 }
